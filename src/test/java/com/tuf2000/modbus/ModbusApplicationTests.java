d2a70c77a700aee5a905bb8b1d0e427e8d7e8c49
mehrdadmoradabadi <mehrdadmoradabadi@yahoo.com>
2024-03-31 17:48:48 +0300

    Init

---
+++ src/test/java/com/tuf2000/modbus/ModbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuf2000.modbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
